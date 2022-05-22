f8086323cbe2814c0370427b5a6b8e59036970ce
VeronikaBogdanovich744 <vera022003@gmail.com>
2022-05-22 22:29:41 +0300

    Added DI library and main program

---
+++ OOP_4/LR4_main/NumberService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LR4_main
+{
+    internal class NumberService:IService
+    {
+        int random;
+        static Random rnd = new Random();
+        public NumberService()
+        {
+            random = rnd.Next(200);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"The number is {random}");
+        }
+    }
+}
---
+++ OOP_4/LR4_main/NumberServiceConsumer.cs
@@ -0,0 +1,16 @@
+﻿namespace LR4_main
+{
+    internal class NumberServiceConsumer: INumberServiceConsumer
+    {
+        NumberService _numberService;
+        public NumberServiceConsumer(NumberService numberService)
+        {
+            _numberService = numberService;
+        }
+
+        public void Print()
+        {
+            _numberService.Print();
+        }
+    }
+}
